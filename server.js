b52549134f0a1c8ccd5bed2f96a87c107e279c8a
Ryan Young <ryanbyoung1@gmail.com>
2023-01-04 15:30:43 -0500

    added GET methods for HTML files and require path variable

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
